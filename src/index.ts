4f9c96c6a3f0a6939daecbc43601301800ea605e
osu-denken-admin <osudenken4dev@gmail.com>
2025-12-11 00:42:23 +0900

    Add 'page' to Access-Control-Allow-Headers

--- src/index.ts
+++ src/index.ts
@@ -176,7 +176,7 @@ export default {
 			return new Response(null, { status: 204, headers: {
 				"Access-Control-Allow-Origin": "*",
 				"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-				"Access-Control-Allow-Headers": "Content-Type, Authorization",
+				"Access-Control-Allow-Headers": "Content-Type, Authorization, page",
 			}});
 		}
 
